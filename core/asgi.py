0f55b635425ea5f6ab7e14d095025a81ce4dea5f
Ian Hart <ianhart7790@gmail.com>
2022-10-16 21:52:10 -0400

    deployment: settings

--- core/asgi.py
+++ core/asgi.py
@@ -7,11 +7,14 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 asgi = get_asgi_application()
 #os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'core.settings'
 
-#django.setup()
 import notification.routing
 import chat.routing
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'core.settings'
+
+
+django.setup()
 
 application = ProtocolTypeRouter({
     "http": asgi,
